cff27f2501fb3aebcd19e957aa9dd07ffd3cbb8a
Daiki Sanno <daiki.sanno@gmail.com>
2013-02-23 14:17:48 +0900

    add.add new test.

---
+++ test/features/step_definitions/steps.rb
@@ -0,0 +1,14 @@
+﻿## -*- coding: utf-8 -*-
+gem 'capybara', '~>2.0'
+gem 'capybara-webkit', '~>0.14.1'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara-webkit'
+Capybara.default_driver = :webkit
+include Capybara::DSL
+前提 /\"(.+)\"を表示/ do |url|
+  visit(url)
+end
+ならば /スクリーンショットを取得し\"(.+)\"に保存/ do |filename|
+  page.save_screenshot "./#{filename}"
+end
