585db56f2f8812ff345065d06f7b0aaae1fe1b1a
Daiki Sanno <daiki.sanno@gmail.com>
2013-02-23 21:18:40 +0900

    mod.modify test to use sinon.js.

--- test2/features/step_definitions/steps.rb
+++ test2/features/step_definitions/steps.rb
@@ -21,3 +21,41 @@ end
 ならば /スクリーンショットを取得し\"(.+)\"に保存/ do |filename|
   page.save_screenshot "./#{filename}"
 end
+前提 'Sinon.JSを利用する' do
+  f = File.open('features/js/sinon.js', "r")
+  sinon = f.read;
+  page.execute_script sinon
+  f.close
+end
+前提 'fakeserverを設定する' do
+  page.execute_script <<-JS
+    server = sinon.fakeServer.create();
+  JS
+end
+もし 'ユーザー名が利用可の応答を受け取る' do
+  # 最後に受けたRequestに対して応答する
+  page.execute_script <<-JS
+    var idx = server.requests.length - 1;
+    server.requests[idx].respond(200, {"Content-Type": "application/json"}, '{"status":"ok"}');
+  JS
+end
+前提 'getJSONにspyを設定する' do
+  page.execute_script <<-JS
+    var spy = sinon.spy($, "getJSON");
+  JS
+end
+ならば /引数\"(.+)\"でgetJSON処理を実行する/ do |name|
+  js = "spy.calledWith('./status', {'name': \'#{name}\'})"
+  ok = page.evaluate_script js
+  unless ok
+    raise "unexpected param"
+  end
+end
+ならば /getJSON処理が(\d+)回実行されている/ do |expect|
+  count = page.evaluate_script <<-JS
+    spy.callCount;
+  JS
+  if count.to_i != expect.to_i
+    raise "unexpected callcount #{count} != #{expect}"
+  end
+end
